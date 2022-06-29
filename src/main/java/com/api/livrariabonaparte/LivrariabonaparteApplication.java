f39481a5fa47e3d1c1f149d2f1d162a9831964a3
Mauro Peixoto <maurograp24@gmail.com>
2022-06-29 17:11:34 -0300

    primeiro commit

---
+++ src/main/java/com/api/livrariabonaparte/LivrariabonaparteApplication.java
@@ -0,0 +1,13 @@
+package com.api.livrariabonaparte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariabonaparteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariabonaparteApplication.class, args);
+	}
+
+}
